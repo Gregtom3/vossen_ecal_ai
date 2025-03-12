f88cb02468ac7aa1eca69fba4235dc574ca0a099
Gregory Matousek <gamatousek@gmail.com>
2025-03-12 13:42:56 -0400

    Test plot

--- tests/test01-plot-photons.py
+++ tests/test01-plot-photons.py
@@ -1,4 +1,20 @@
-def test():
-    print("Success!")
+import numpy as np
+import matplotlib.pyplot as plt
 
-test()
+def test_gaussian_histogram():
+    # Generate 1000 random samples from a Gaussian distribution
+    data = np.random.normal(loc=0, scale=1, size=1000)
+    
+    # Create a histogram
+    plt.figure()
+    plt.hist(data, bins=30, color='blue', alpha=0.7)
+    plt.title("Gaussian Histogram")
+    plt.xlabel("Value")
+    plt.ylabel("Frequency")
+    
+    # Save the plot to a file
+    plt.savefig("gaussian_histogram.png")
+    plt.close()
+
+if __name__ == '__main__':
+    test_gaussian_histogram()
